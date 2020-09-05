be13e64b283dce3e2a2a0bee492b78d23a4c8b0e
César Mejía <cesarmejiaguzman@gmail.com>
2020-09-05 01:33:17 -0500

    Footer done.

---
+++ html/design/scripts/scripts.js
@@ -0,0 +1,1 @@
+!function(){"use strict";"serviceWorker"in navigator&&navigator.serviceWorker.register("sw.js").then(function(e){console.log("Service worker registered.")}).catch(function(e){console.log("Can not find service worker.")})}();
